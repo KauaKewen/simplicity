9db85223f52da44adb323e4a0e74e39c77385b21
kewen <kauakewen1010@hotmail.com>
2024-05-02 15:29:25 -0300

    Update contato.js

--- js/contato.js
+++ js/contato.js
@@ -12,5 +12,6 @@ const mensagem = formulario.querySelector("#status");
 
 /* Detectando quando o botão de buscar CEP é acionado */
 botaoBuscar.addEventListener("click", function(event){
+    /* Anular o comportamento padrão de redirecionamento/recarregamento da página. Sempre acontece ao trabalhar com <a> e <form> */
     event.preventDefault();
 })
